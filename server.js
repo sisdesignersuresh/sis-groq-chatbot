da48497ec05d560f3b1e643a843285c27892a526
sisdesignersuresh <sis.designersuresh@gmail.com>
2026-05-28 16:02:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post('/chat', async (req, res) => {
 
                 body: JSON.stringify({
 
-                    model: 'llama3-8b-8192',
+                    model: 'llama-3.1-8b-instant',
 
                     messages: [
 
